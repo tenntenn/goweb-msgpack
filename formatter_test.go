9ca0922c15f0a94d23a6e7c5d1c0726362dc86e6
Takuya Ueda <uedatakuya@gmail.com>
2012-08-06 17:19:08 +0900

    Add test code for MsgpackFormatter.Format().

---
+++ formatter_test.go
@@ -0,0 +1,43 @@
+package gowebmsgpack
+
+import (
+    "testing"
+    "bytes"
+    "net/http"
+    "net/http/httptest"
+    msgpack "github.com/ugorji/go-msgpack"
+    "code.google.com/p/goweb/goweb"
+)
+
+// Test of gowebmsgpack.MsgpackFormatter.Format().
+func TestFormatterFormat(t *testing.T) {
+
+    // Create and regist formatter
+    formatter := new(MsgpackFormatter)
+    goweb.AddFormatter(formatter)
+
+    // Create context 
+    r, _ := http.NewRequest("GET", "http://localhsot:8080", nil)
+    w := httptest.NewRecorder()
+    pathPrams := goweb.ParameterValueMap(make(map[string]string))
+    cx := &goweb.Context{r, w, pathPrams, MSGPACK_FORMAT}
+
+    data := []int{1, 2, 3}
+    result := struct {
+        C string
+        S int
+        D interface{}
+        E []string
+    }{
+        "",
+        http.StatusOK,
+        data,
+        nil,
+    }
+    expect, _ := msgpack.Marshal(result, nil)
+    cx.RespondWithData(data)
+    actual := w.Body.Bytes()
+    if !bytes.Equal(expect, actual) {
+        t.Errorf("expect %v but actual %v.", expect, actual)
+    }
+}
